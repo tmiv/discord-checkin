e2b675012c054bc8917b856a4f4d1aa8b552a529
Thomas Miller <tmiv74@gmail.com>
2023-12-30 09:24:17 -0800

    preconfigured version

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -9,12 +11,22 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/rs/cors"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/idtoken"
+	"google.golang.org/api/option"
 )
 
 const DiscordTokenURI = "https://discord.com/api/oauth2/token"
 const DiscordUserInfoURI = "https://discord.com/api/users/@me"
+
+var (
+	RedirectURI = os.Getenv("REDIRECT_URI")
+	IdToken     *oauth2.Token
+)
 
 type DiscordTokenResponse struct {
 	TokenType    string `json:"token_type"`
@@ -56,6 +68,42 @@ func GetDiscordUserInfo(accessToken string) (*DiscordUserInfo, error) {
 	return &userInfo, nil
 }
 
+func initIdToken(ctx context.Context, aud string) error {
+	credentials, err := google.FindDefaultCredentials(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to generate default credentials: %w", err)
+	}
+
+	ts, err := idtoken.NewTokenSource(ctx, aud, option.WithCredentials(credentials))
+	if err != nil {
+		return fmt.Errorf("failed to create NewTokenSource: %w", err)
+	}
+
+	tok, err := ts.Token()
+	if err != nil {
+		return fmt.Errorf("failed to receive token: %w", err)
+	}
+
+	IdToken = tok
+
+	return nil
+}
+
+func getToken(ctx context.Context) (string, error) {
+	conf := &oauth2.Config{}
+	if IdToken.Expiry.Before(time.Now()) {
+		src := conf.TokenSource(ctx, IdToken)
+		newToken, err := src.Token() // this actually goes and renews the tokens
+		if err != nil {
+			return "", err
+		}
+		if newToken.AccessToken != IdToken.AccessToken {
+			IdToken = newToken
+		}
+	}
+	return IdToken.AccessToken, nil
+}
+
 func DiscordLogin(w http.ResponseWriter, r *http.Request) {
 	codeArray := r.URL.Query()["code"]
 	if len(codeArray) < 1 {
@@ -91,15 +139,15 @@ func DiscordLogin(w http.ResponseWriter, r *http.Request) {
 	data.Set("grant_type", "authorization_code")
 	data.Set("code", code)
 	data.Set("redirect_uri", d_redirect_uri)
-	tok_req, err := http.NewRequest(http.MethodPost, DiscordTokenURI, strings.NewReader(data.Encode()))
+	dis_tok_req, err := http.NewRequest(http.MethodPost, DiscordTokenURI, strings.NewReader(data.Encode()))
 	if err != nil {
 		log.Printf("Error %v", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	tok_req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	tok_req.Header.Add("Accept-Encoding", "application/x-www-form-urlencoded")
-	tok_resp, err := http.DefaultClient.Do(tok_req)
+	dis_tok_req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	dis_tok_req.Header.Add("Accept-Encoding", "application/x-www-form-urlencoded")
+	tok_resp, err := http.DefaultClient.Do(dis_tok_req)
 	if err != nil {
 		log.Printf("Error %v", err)
 		w.WriteHeader(http.StatusBadRequest)
@@ -130,19 +178,62 @@ func DiscordLogin(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	w.Header().Add("Token", "Generated Token"+userInfo.Id)
-	http.Redirect(w, r, "http://localhost:8090/v1/Copyback", http.StatusSeeOther)
-}
 
-func Copyback(w http.ResponseWriter, r *http.Request) {
-	log.Printf("CB\n")
-	log.Printf("%+v\n", r.Header)
+	gtok, err := getToken(r.Context())
+	if err != nil {
+		log.Printf("Error generating token %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	niv_tok_map := map[string]string{
+		"user_id":   userInfo.Id,
+		"avatar":    userInfo.Avatar,
+		"user_name": userInfo.UserName,
+		"aud":       os.Getenv("NIV_TOKEN_AUDIENCE"),
+	}
+	niv_tok_data, err := json.Marshal(niv_tok_map)
+	if err != nil {
+		log.Printf("Error Marshaling niv data %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	niv_tok_req, err := http.NewRequest(http.MethodPost, os.Getenv("NIV_TOKEN_URL"), bytes.NewReader(niv_tok_data))
+	if err != nil {
+		log.Printf("Error generating niv token req %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	niv_tok_req.Header.Add("Content-Type", "application/json")
+	niv_tok_req.Header.Add("authorization", "Bearer "+gtok)
+	niv_tok_resp, err := http.DefaultClient.Do(niv_tok_req)
+	if err != nil {
+		log.Printf("Error generating niv token %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if niv_tok_resp.StatusCode < 200 || niv_tok_resp.StatusCode >= 300 {
+		log.Printf("Error generating niv token Bad Status %d\n", niv_tok_resp.StatusCode)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer niv_tok_resp.Body.Close()
+	niv_tok, err := io.ReadAll(niv_tok_resp.Body)
+	if err != nil {
+		log.Printf("Error reading niv token %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Printf("URL: %s?token=%s\n", RedirectURI, string(niv_tok))
+	http.Redirect(w, r, RedirectURI+"?token="+string(niv_tok), http.StatusSeeOther)
 }
 
 func main() {
+	err := initIdToken(context.Background(), os.Getenv("G_TOKEN_AUDIENCE"))
+	if err != nil {
+		log.Fatalf("Could not generate id token %v\n", err)
+	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v1/DiscordLogin", DiscordLogin)
-	mux.HandleFunc("/v1/Copyback", Copyback)
 
 	handler := cors.Default().Handler(mux)
 	http.ListenAndServe("0.0.0.0:8080", handler)
